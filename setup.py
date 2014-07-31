d2cd0d1cb48c1548bc5bd10d75b6b110f683b46d
Ewald Zietsman <ewald@siyavula.com>
2014-07-31 12:13:57 +0200

    First commit for new repo, previous history in cnxml-validator repo

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='Bookbuilder',
+      version='0.1',
+      description='Tools for generating Siyavula\'s publishing formats',
+      author='Ewald Zietsman',
+      author_email='ewald@siyavula.com',
+      url='http://www.siyavula.com/',
+      packages=['Bookbuilder'],
+      scripts=['bookbuilder'],
+      install_requires=['docopt',
+                        'lxml',
+                        'termcolor',
+                        'siyavula.transforms'],
+      )
