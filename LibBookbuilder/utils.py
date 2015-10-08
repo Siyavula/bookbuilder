5688d6d72cbb1942ff96aac6a7db08125de42c42
HeatherW <heather@siyavula.com>
2015-10-08 14:45:18 +0200

    added html5 transforms to bookbuilder

--- LibBookbuilder/utils.py
+++ LibBookbuilder/utils.py
@@ -146,8 +146,8 @@ class TocElement(object):
         ''' Return object as etree element'''
         li = etree.Element('li')
         a = etree.Element('a')
-        a.attrib['href'] = '{}#{}'.format(self.filename,
-                                              self.element.attrib['id'])
+        #a.attrib['href'] = '{}#{}'.format(self.filename,
+                                              #self.element.attrib['id'])
         a.text = self.element.text
         li.append(a)
         ol = etree.Element('ol')
@@ -168,6 +168,23 @@ def add_unique_ids(html):
     headings = ['h1', 'h2']
     for heading in headings:
         for htag in html.findall('.//div/{}'.format(heading)):
+            _class = htag.getparent().attrib.get('class')
+            if _class not in ['chapter', 'section', 'exercises']:
+                continue
+            current_id = htag.attrib.get('id')
+            if (current_id not in IDs) and (current_id is not None):
+                IDs.append(current_id)
+                continue
+
+            # it doesn't have an ID
+            new_id = "toc-id-{}".format(_id)
+            while new_id in IDs:
+                _id += 1
+                new_id = "toc-id-{}".format(_id)
+            IDs.append(new_id)
+            htag.attrib['id'] = new_id
+            _id += 1
+        for htag in html.findall('.//section/{}'.format(heading)):
             _class = htag.getparent().attrib.get('class')
             if _class not in ['chapter', 'section', 'exercises']:
                 continue
