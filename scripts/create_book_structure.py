e0709afedc06a6308d1483065321e21362ea9539
HeatherW <heather@siyavula.com>
2016-03-17 16:46:45 +0200

    make sure file conforms to style guidelines

--- scripts/create_book_structure.py
+++ scripts/create_book_structure.py
@@ -9,6 +9,7 @@ path = '/home/heather/Desktop/books/physical-sciences-12/english'
 file_list = os.listdir(path)
 file_list.sort()
 
+
 class BookStructureCreation():
     """
     This class extracts the book structure information into a yaml file
@@ -19,20 +20,20 @@ class BookStructureCreation():
         self.file_contents = ''
         self.book_data = 'grade: 12 \n'
         self.book_data += 'subject: science \n'
-    
+
     def create_structure(self, write_back_to_file_boolean=False):
         self.book_data += self.section_chapter_extract(file_list, path)
-        
+
         if not write_back_to_file_boolean:
             return
-        
+
         self.write_to_yaml_file(self.book_data)
-    
+
     def section_chapter_extract(self, file_list, path):
         self.file_contents += 'chapters: \n'
         for file_name in self.file_list:
             full_file_name = '{}/{}'.format(self.path, file_name)
-            
+
             # Skip directories
             if os.path.isdir(full_file_name):
                 continue
@@ -43,17 +44,19 @@ class BookStructureCreation():
                 continue
             if file_name[0] not in ['0', '1', '2', '3']:
                 continue
-            
+
             xml = etree.parse(full_file_name, etree.HTMLParser())
             chapter_number = int(file_name[:2])
-        
             section_list = []
             section_number = 1
+
             for section in xml.findall('.//section[@type]'):
                 if section.attrib['type'] == 'chapter':
                     chapter_title = section.find('.//title')
-                    self.file_contents += '  - number: {} \n'.format(chapter_number)
-                    self.file_contents += '    title: {} \n'.format(chapter_title.text)
+                    self.file_contents += '  - number: {} \n'.format(
+                        chapter_number)
+                    self.file_contents += '    title: {} \n'.format(
+                        chapter_title.text)
                 elif section.attrib['type'] == 'section':
                     title = section.find('.//title')
                     title_text = title.text
@@ -66,11 +69,11 @@ class BookStructureCreation():
             for i in section_list:
                 self.file_contents += '      - {} \n'.format(i)
         return self.file_contents
-    
+
     def write_to_yaml_file(self, file_contents):
-        # write the contents of each dictionary and list to a file
         with open('test.txt', 'w') as file:
             file.write(file_contents)
 
-book_structure = BookStructureCreation(file_list, path)
-book_structure.create_structure(True)
+if __name__ == "__main__":
+    book_structure = BookStructureCreation(file_list, path)
+    book_structure.create_structure(True)
