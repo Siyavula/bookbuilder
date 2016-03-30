b339db175f9d7aa2bedd03728fa45414eac6e4c7
HeatherW <heather@siyavula.com>
2016-03-30 11:19:32 +0200

    fix items from review

--- scripts/create_book_structure.py
+++ scripts/create_book_structure.py
@@ -1,27 +1,34 @@
-'''
-This code sets up a yaml file with the structure of a book
-'''
+"""
+Usage:
+    create_book_structure.py <path_name>
+
+Options:
+    path_name gives the path to the files that need to be numbered
+
+Examples:
+    create_book_structure.py '/home/books/grade-10-mathslit-latex/english'
+
+"""
 import os
+import sys
+from docopt import docopt
 from lxml import etree
 import yaml
-
-path = '/home/heather/Desktop/books/grade-10-mathslit-latex/english'
-file_list = os.listdir(path)
-file_list.sort()
 
 
 class BookStructureCreation():
     """
     This class extracts the book structure information into a yaml file
     """
-    def __init__(self, file_list, path):
-        self.file_list = file_list
+    def __init__(self, path):
         self.path = path
+        self.file_list = os.listdir(self.path)
+        self.file_list.sort()
         self.file_contents = ''
         self.book_data = ''
 
     def create_structure(self, write_back_to_file_boolean=False):
-        self.book_data += self.section_chapter_extract(file_list, path)
+        self.book_data += self.section_chapter_extract(self.file_list, self.path)
 
         if not write_back_to_file_boolean:
             return
@@ -39,9 +46,9 @@ class BookStructureCreation():
 
             # we need to remove files that do not contain xml
             # and files that do not start with a number
-            if file_name[-9:] != 'cnxmlplus':
+            if not file_name.endswith('cnxmlplus'):
                 continue
-            if file_name[0] not in ['0', '1', '2', '3']:
+            if not file_name[0].isdigit():
                 continue
 
             xml = etree.parse(full_file_name, etree.HTMLParser())
@@ -54,19 +61,17 @@ class BookStructureCreation():
                     chapter_title = section.find('.//title')
                     self.file_contents += '  - number: {} \n'.format(
                         chapter_number)
-                    self.file_contents += '    title: {} \n'.format(
-                        chapter_title.text)
+                    self.file_contents += '    title: ' + chapter_title.text + ' \n'
                 elif section.attrib['type'] == 'section':
                     title = section.find('.//title')
                     title_text = title.text
                     title_text = title_text.replace(':', '":"')
-                    section_title = '{}.{} {}'.format(
-                        chapter_number, section_number, title_text)
+                    section_title = str(chapter_number) + '.' + str(section_number) + ' ' + title_text
                     section_number += 1
                     section_list.append(section_title)
             self.file_contents += '    sections: \n'
             for i in section_list:
-                self.file_contents += '      - {} \n'.format(i)
+                self.file_contents += '      - ' + i + ' \n'
         return self.file_contents
 
     def write_to_yaml_file(self, file_contents):
@@ -74,5 +79,6 @@ class BookStructureCreation():
             file.write(file_contents)
 
 if __name__ == "__main__":
-    book_structure = BookStructureCreation(file_list, path)
+    path = docopt(__doc__)['<path_name>']
+    book_structure = BookStructureCreation(path)
     book_structure.create_structure(True)
--- scripts/tests/book_structure_tests.py
+++ scripts/tests/book_structure_tests.py
@@ -10,14 +10,13 @@ from create_book_structure import BookStructureCreation
 class BookStructureTests(TestCase):
     def setUp(self):
         path = '/home/heather/Desktop/books/bookbuilder/scripts/tests/sample-files-for-testing/cnxmlplus_files'
-        file_list = os.listdir(path)
-        file_list.sort()
-        self.book_structure = BookStructureCreation(file_list, path)
+        self.book_structure = BookStructureCreation(path)
 
     def test_book_structure_data(self):
         self.book_structure.create_structure(False)
         book_data = yaml.load(self.book_structure.book_data)
-        with open('sample-files-for-testing/book_structure_test.yaml', 'r') as file:
+        test_data_path = '/home/heather/Desktop/books/bookbuilder/scripts/tests/sample-files-for-testing/book_structure_test.yaml'
+        with open(test_data_path, 'r') as file:
             test_data = yaml.load(file)
         assert book_data == test_data
         
